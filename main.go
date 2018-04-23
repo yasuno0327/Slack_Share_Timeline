1d320b0fcef98ce947bef686162b27434d952d30
yasuno0327 <5315076@ed.tus.ac.jp>
2018-04-23 21:39:40 +0900

    Refactor

--- main.go
+++ main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-  "log"
-  "os"
-  "fmt"
+	"fmt"
+	"log"
+	"os"
 
-  "github.com/nlopes/slack"
-  "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
+	"github.com/nlopes/slack"
 )
 
 func run(api *slack.Client) int {
-  rtm := api.NewRTM()
-  go rtm.ManageConnection()
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
 
-  for {
-    select {
-    case msg := <- rtm.IncomingEvents:
-      switch ev := msg.Data.(type) {
-      case *slack.MessageEvent:
-        fmt.Println(ev)
-        rtm.SendMessage(rtm.NewOutgoingMessage(ev.Msg.Text, os.Getenv("SLACK_SAMPLE_TIMELINE")))
+	for {
+		select {
+		case msg := <-rtm.IncomingEvents:
+			switch ev := msg.Data.(type) {
+			case *slack.MessageEvent:
+				fmt.Println(ev)
+				rtm.SendMessage(rtm.NewOutgoingMessage(ev.Msg.Text, os.Getenv("SLACK_SAMPLE_TIMELINE")))
 
-      case *slack.InvalidAuthEvent:
-        log.Print("Invalid credentials")
-        return 1
-      }
-    }
-  }
+			case *slack.InvalidAuthEvent:
+				log.Print("Invalid credentials")
+				return 1
+			}
+		}
+	}
 }
 
 func main() {
-  Env_load()
-  api := slack.New(os.Getenv("SLACK_API_TOKEN"))
-  os.Exit(run(api))
+	Env_load()
+	api := slack.New(os.Getenv("SLACK_API_TOKEN"))
+	os.Exit(run(api))
 }
