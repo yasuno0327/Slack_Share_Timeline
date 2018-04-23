c2453104b86c2ba139fcd67f786a4c58730b08d0
楊野 良智 <yasunoyoshimoto@LAB-N870.local>
2018-04-23 15:20:52 +0900

    Initialize App

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "log"
+  "os"
+  "fmt"
+
+  "github.com/nlopes/slack"
+  "github.com/joho/godotenv"
+)
+
+func run(api *slack.Client) int {
+  rtm := api.NewRTM()
+  go rtm.ManageConnection()
+
+  for {
+    select {
+    case msg := <- rtm.IncomingEvents:
+      switch ev := msg.Data.(type) {
+      case *slack.MessageEvent:
+        fmt.Println(ev)
+        rtm.SendMessage(rtm.NewOutgoingMessage(ev.Msg.Text, os.Getenv("SLACK_SAMPLE_TIMELINE")))
+
+      case *slack.InvalidAuthEvent:
+        log.Print("Invalid credentials")
+        return 1
+      }
+    }
+  }
+}
+
+func main() {
+  Env_load()
+  api := slack.New(os.Getenv("SLACK_API_TOKEN"))
+  os.Exit(run(api))
+}
